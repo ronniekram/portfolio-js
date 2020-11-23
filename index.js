e4eeb192984ab53f99021a31bba58c4158c899eb
Arynn Boniface <arynn.boniface@gmail.com>
2020-11-23 14:44:37 -0500

    added event listeners and function skeletons

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const content = document.getElementById("main-content");
+const about = document.getElementById("about-link");
+const portfolio = document.getElementById("portfolio-link");
+const contact = document.getElementById("contact-link");
+const home = document.getElementById("home-link");
 
+document.addEventListener("DOMContentLoaded", () => {
+    about.addEventListener("click", renderAbout());
+    contact.addEventListener("click", renderContact());
+    portfolio.addEventListener("click", renderPortfolio());
+    home.addEventListener("click", renderHome());
+})
 
 function renderAbout() {
-
-}
-
-function renderPortfolio() {
+    clearPage();
 
 }
 
 function renderContact() {
-    
+    clearPage();
+
+}
+
+function renderPortfolio() {
+    clearPage();
+}
+
+function renderHome() {
+    clearPage();
+
+}
+
+function clearPage() {
+    content.innerHTML = ""
 }
